64015d8987d620546d2c65ab68756a2c0c811817
CorZoon <72453908+CorZoon@users.noreply.github.com>
2022-04-30 13:46:11 +0200

    Call optimization function external with output

---
+++ Run_function_external.py
@@ -0,0 +1,12 @@
+from working_testing import perform_experiments
+
+KNAPSACK_PACKING_PROBLEM_TYPE = "KnapsackPacking"
+KNAPSACK_PACKING_PROBLEM_DIR = "../Output/Problems/CustomKnapsackPacking/Test/"
+problem_type, output_dir = KNAPSACK_PACKING_PROBLEM_TYPE, KNAPSACK_PACKING_PROBLEM_DIR
+load_experiments = False
+
+
+
+if __name__ == '__main__':
+    placed_connectors = perform_experiments(KNAPSACK_PACKING_PROBLEM_TYPE,KNAPSACK_PACKING_PROBLEM_DIR,load_experiments)
+    print(placed_connectors)
